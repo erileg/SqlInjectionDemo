12125d7c5bdba394e4d88e3fb8f5ab25f63f3886
erle <erik.legenhausen@gmail.com>
2016-11-26 20:50:23 +0100

    better filter handling

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,9 @@ const mysql = require("mysql");
 module.exports = app => {    
     // show all users
     app.get('/users', (req, res, next) => {
-        const filter = req.query.filter ? req.query.filter : ""; 
-        //const filter = req.params.filter ? req.params.filter : ""; 
-        const query = `SELECT lastname, firstname, email FROM users WHERE INSTR(lastname, ?) > 0 ORDER BY lastname`;
+        const filterClause = req.query.filter ? "INSTR(CONCAT(firstname, '|', lastname), ?)" : "1=1"; 
+        //const filter = req.params.filter ||  ""; 
+        const query = `SELECT lastname, firstname, email FROM users WHERE ${filterClause} > 0 ORDER BY lastname`;
         //console.log(`query: ${query}`);
         app.queryDb(query, [req.query.filter], (err, records) => {
             if (!err){
