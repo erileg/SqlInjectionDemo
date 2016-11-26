6d1a90d2c95b8dae0d0d9f277e7299e6dbf1450c
erle <erik.legenhausen@gmail.com>
2016-11-26 23:36:48 +0100

    clean up

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,9 @@ module.exports = app => {
     // show all users
     app.get('/users', (req, res, next) => {
         const filterClause = req.query.filter ? "INSTR(CONCAT(firstname, '|', lastname, '|', email), ?)" : "true";
-        //const filter = req.params.filter ||  ""; 
         const query = `SELECT id, lastname, firstname, email FROM users WHERE ${filterClause} > 0 ORDER BY lastname`;
         //console.log(`query: ${query}`);
+        
         app.queryDb(query, [req.query.filter]).then(users => {
             res.render("users", { "title": "users", "users": users });
         }).catch(err => {
