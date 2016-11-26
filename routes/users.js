95155ee5bb597e0d32b5568e5789f88c29dd783d
erle <erik.legenhausen@gmail.com>
2016-11-26 23:49:59 +0100

    added orderCol query param

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,8 @@ module.exports = app => {
     // show all users
     app.get('/users', (req, res, next) => {
         const filterClause = req.query.filter ? "INSTR(CONCAT(firstname, '|', lastname, '|', email), ?)" : "true";
-        const query = `SELECT id, lastname, firstname, email FROM users WHERE ${filterClause} > 0 ORDER BY lastname`;
+        const orderCol = req.query.orderby || "lastname";
+        const query = `SELECT id, lastname, firstname, email FROM users WHERE ${filterClause} > 0 ORDER BY ${orderCol}`;
         //console.log(`query: ${query}`);
         
         app.queryDb(query, [req.query.filter]).then(users => {
