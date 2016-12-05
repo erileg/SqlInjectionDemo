12b46e3d06a6f8e5fd7e4279df1136e0bcf34e52
erle <erik.legenhausen@gmail.com>
2016-12-05 21:54:01 +0100

    added user edit form

--- routes/users.js
+++ routes/users.js
@@ -15,6 +15,11 @@ module.exports = app => {
         });
     });
 
+    // edit user
+    app.get('/user', (req, res, next) => {
+        res.render("useredit", { "title": "Edit"});
+    });
+    
     // create user
     app.post('/users', (req, res, next) => {
         const query = `INSERT into users (
@@ -27,12 +32,33 @@ module.exports = app => {
             city,
             zipcode,
             street,
+            streetnumber,
             creditcardcmp,
             creditcardpan,
             creditcardcvv
-            VALUES(
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        
+        const userdata = [
+                req.body.username,
+                req.body.password,
+                req.body.email,
+                req.body.firstname,
+                req.body.lastname,
+                req.body.country,
+                req.body.city,
+                req.body.zipcode,
+                req.body.street,
+                req.body.streetnumber,
+                req.body.creditcardcmp,
+                req.body.creditcardpan,
+                req.body.creditcardcvv            
+        ]
 
-            )`;
+        app.queryDb(query, userdata).then(users => {
+            res.redirect('/users');
+        }).catch(err => {
+            next();
+        });
     });
 
     // delete a user
--- server.js
+++ server.js
@@ -1,46 +1,46 @@
 // modules
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const path = require("path");
+const bodyParser = require('body-parser');
+const path = require('path');
 
-require("console-stamp")(console, {
-  pattern: "dd.mm.yyyy HH:MM:ss",
+require('console-stamp')(console, {
+  pattern: 'dd.mm.yyyy HH:MM:ss',
   label: false,
   colors: {
-    stamp: "yellow",
-    label: "white",
-    metadata: "green"
+    stamp: 'yellow',
+    label: 'white',
+    metadata: 'green'
   }
 });
 
 // read configuration
-const config = require("./config");
+const config = require('./config');
 
 // register MySql query function
-app.queryDb = require("./modules/dbConnection").queryDb;
+app.queryDb = require('./modules/dbConnection').queryDb;
 
 // setup express to use pug template engine
-app.set("view engine", "pug");
+app.set('view engine', 'pug');
 app.locals.pretty = true;
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // body parser
 app.use(bodyParser.json());
 
 // middleware for all requests
-require("./routes/all")(app);
+require('./routes/all')(app);
 
 // init saleport routes
-require("./routes/index")(app);
+require('./routes/index')(app);
 
 // users
-require("./routes/users")(app);
+require('./routes/users')(app);
 
 // error handling
-require("./routes/error")(app);
+require('./routes/error')(app);
 
 // start server
 var server = app.listen(config.server.port, config.server.address, () => {
-  console.log("Service listening on %s:%s...", server.address().address, server.address().port);
+  console.log('Service listening on %s:%s...', server.address().address, server.address().port);
 });
