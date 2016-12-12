d9d61f72e4c403880d5df61dc0545f121bb87e9e
Erik Legenhausen <erik.legenhausen@engram.de>
2016-12-12 10:56:34 +0100

    better handling of orderby param

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,20 @@ module.exports = app => {
     // show all users
     app.get('/users', (req, res, next) => {
         const filterVal = req.query.filter;
-        const filterClause = filterVal ? "INSTR(CONCAT(firstname, '|', lastname, '|', email), ?) > 0" : "true";
+        let filterClause = "true";
+        const queryParams = [];
+
+        if(filterVal){
+            filterClause = "INSTR(CONCAT(firstname, '|', lastname, '|', email), ?) > 0";
+            queryParams.push(filterVal);    
+        }
 
         const orderCol = req.query.orderby || "lastname";
+        queryParams.push(orderCol);
 
         const query = `SELECT id, lastname, firstname, email FROM users WHERE ${filterClause} ORDER BY ?`;
 
-        app.queryDb(query, [filterVal, orderCol]).then(users => {
+        app.queryDb(query,queryParams).then(users => {
             res.render("users", { "title": "User List", "users": users, "filter": req.query.filter });
         }).catch(err => {
             next();
