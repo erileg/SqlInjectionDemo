5473084497a0d007c919534bc3b5aa3843e9ad01
Erik Legenhausen <erik.legenhausen@engram.de>
2016-12-13 17:59:47 +0100

    typo

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // logging
-app.use(morgan('common'))
+app.use(morgan('common'));
 
 // index route
 require('./routes/index')(app);
