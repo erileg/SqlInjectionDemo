e9b19d20b2384fec0cf28b8f0daff0cf23b92da5
erle <erik.legenhausen@gmail.com>
2017-01-11 22:44:43 +0100

    moved public html files to subfolder 'html'

--- routes/all.js
+++ routes/all.js
@@ -3,11 +3,11 @@ const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 module.exports = app => {
     require('./index')(app);
     require('./login')(app);
-    require('./public/customers')(app);
+    require('./public/html/customers')(app);
     require('./public/rest/customers')(app);
 
     app.use('/protected', ensureLoggedIn('/login'));
-    require('./protected/customers')(app);
+    require('./protected/html/customers')(app);
     require('./protected/rest/customers')(app);
     
     require('./error')(app);
