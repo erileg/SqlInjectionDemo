3c6520be28225f21007f343498887474a74789cb
erle <erik.legenhausen@gmail.com>
2017-01-12 21:29:14 +0100

    refactoring

--- routes/rest/customers.js
+++ routes/rest/customers.js
@@ -3,22 +3,22 @@ const queryDb = require('../../modules/dbConnection').queryDb;
 module.exports = app => {
     app.get('/public/rest/customers', (req, res, next) => {
         const query = `SELECT id, username, lastname, firstname, email FROM customers WHERE role = 'CUSTOMER'`;
-        listCustomers(query, [], res, next);
+        sendCustomers(query, [], res, next);
     });
 
     app.get('/public/rest/customers/:id', (req, res, next) => {
         const query = `SELECT id, username, lastname, firstname, email FROM customers WHERE role = 'CUSTOMER' AND id = ?`;
-        listCustomers(query, [req.params.id], res, next);
+        sendCustomers(query, [req.params.id], res, next);
     });
 
     app.get('/protected/rest/customers', (req, res, next) => {
         const query = 'SELECT * FROM customers';
-        listCustomers(query, [], res, next);
+        sendCustomers(query, [], res, next);
     });
 
     app.get('/protected/rest/customers/:id', (req, res, next) => {
         const query = `SELECT * FROM customers WHERE id = ?`;
-        listCustomers(query, [req.params.id], res, next);
+        sendCustomers(query, [req.params.id], res, next);
     });
 
     app.delete('/protected/customers/:id', (req, res, next) => {
@@ -31,7 +31,7 @@ module.exports = app => {
     });
 }
 
-const listCustomers = function (query, params, res, next) {
+const sendCustomers = function (query, params, res, next) {
     queryDb(query, params).then(customers => {
         if (customers && customers.length == 1) {
             res.json(customers[0]);
